e9daf5826fe7547da0f23e4f3b567299333a3ce3
Nguyễn Chương Hào <102464075+chwonghao@users.noreply.github.com>
2025-12-08 16:28:33 +0700

    change browser router and add auto deploy file

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import theme from './theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter base={process.env.PUBLIC_URL}>
+    <BrowserRouter base="/booknest-demo">
       <ThemeProvider theme={theme}>
         <NotificationProvider>
           <AuthProvider>
